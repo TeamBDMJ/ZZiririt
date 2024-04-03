e64a03286c14718bfdef96a3faa4e41419c2afce
Moveuk <v_donguk@naver.com>
2024-04-03 15:57:21 +0900

    Revert "ZRR-109 Fix: 설정으로 인한 local test 실패 수정"
    This reverts commit c1c0b9d57276fe2103defc024ffa3042eee8159d.

--- src/main/kotlin/kr/zziririt/zziririt/infra/postgres/DataSourceConfiguration.kt
+++ src/main/kotlin/kr/zziririt/zziririt/infra/postgres/DataSourceConfiguration.kt
@@ -4,12 +4,14 @@ import com.zaxxer.hikari.HikariDataSource
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder
-import org.springframework.context.annotation.*
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.DependsOn
+import org.springframework.context.annotation.Primary
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy
 import javax.sql.DataSource
 
 
-@Profile("prod")
 @Configuration
 class DataSourceConfiguration {
     @Bean(PRIMARY_DATASOURCE)
