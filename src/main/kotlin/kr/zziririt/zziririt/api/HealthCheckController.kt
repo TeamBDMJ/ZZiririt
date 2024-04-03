085e096cbc499cacadaeaad25a58e7d002d6e317
Moveuk <v_donguk@naver.com>
2024-04-03 15:57:21 +0900

    Revert "ZRR-109 Fix: DB CQRS 도입 및 DB AWS RDS Aurora 이전"
    This reverts commit 7b08418db11c27ae0bf93c0ecaec306f40a8f5d6.

--- src/main/kotlin/kr/zziririt/zziririt/api/HealthCheckController.kt
+++ src/main/kotlin/kr/zziririt/zziririt/api/HealthCheckController.kt
@@ -1,37 +1,11 @@
 package kr.zziririt.zziririt.api
 
-import io.github.oshai.kotlinlogging.KotlinLogging
-import org.springframework.transaction.annotation.Transactional
+import kr.zziririt.zziririt.domain.member.repository.SocialMemberRepository
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import java.sql.Connection
-import java.sql.SQLException
-import javax.sql.DataSource
-
-private val kLogger = KotlinLogging.logger {}
 
 @RestController
-class HealthCheckController(
-    private val dataSource: DataSource
-) {
+class HealthCheckController(private val socialMemberRepository: SocialMemberRepository) {
     @GetMapping("/ping")
     fun ping() = "pong"
-
-    @GetMapping("/tr-w")
-    @Transactional
-    @Throws(SQLException::class)
-    fun primary() {
-        val connection: Connection = dataSource.connection
-
-        kLogger.info { "${"primary url : {}"} ${connection.metaData.url}" }
-    }
-
-    @GetMapping("/tr-ro")
-    @Transactional(readOnly = true)
-    @Throws(SQLException::class)
-    fun secondary() {
-        val connection: Connection = dataSource.connection
-
-        kLogger.info { "${"secondary url : {}"} ${connection.metaData.url}" }
-    }
 }
