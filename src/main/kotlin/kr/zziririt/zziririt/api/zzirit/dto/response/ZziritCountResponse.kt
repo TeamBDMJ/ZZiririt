b8e891f6c0bc0c94af91b7d94549281248aff630
doojoo9999 <parkbr95@naver.com>
2024-04-04 03:27:34 +0900

    ZRR-113 Feat: Popular Posts(TOP5)
    1. zziritController
    - 24시간 내 가장 많은 찌릿을 받은 상위 5개의 Post를 가져올 수 있음
    2. ZziritCountResponse
    - 반환정보: postId, zziritCount, postTitle, boardUrl
    3. QueryDSL
    - 최근 24시간 내 작성된 게시글 중 zzirit Count가 높은 순서대로 게시글을 n개 출력함
    - 5개로 설정되어 있으나, 10개로 변경 할 경우 limitCount 수정
    4. ZziritService
    - 1시간 간격으로 rank를 update함 (00시 기준 24시간이 아니라 현재 시각 기준 24시간으로 설정하기 위함)
    - redis 문제 등으로 인해 cache가 없는 경우 다시 update를 진행하여 값을 생성함

---
+++ src/main/kotlin/kr/zziririt/zziririt/api/zzirit/dto/response/ZziritCountResponse.kt
@@ -0,0 +1,8 @@
+package kr.zziririt.zziririt.api.zzirit.dto.response
+
+data class ZziritCountResponse(
+    val postId: Long,
+    val zziritCount: Long,
+    val postTitle: String,
+    val boardUrl: String,
+)
